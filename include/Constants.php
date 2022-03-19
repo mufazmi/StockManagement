d9d04f2fc73eed2102f0937578d8bea3fdfa752a
mufazmi <info.mufazmi@gmail.com>
2022-03-20 02:04:07 +0530

    Project Updated

---
+++ include/Constants.php
@@ -0,0 +1,8 @@
+<?php
+define('API_URL', 'http://localhost/youtube/api/public/');		//Don't Forgot to put slash at the end of url
+define('WEBSITE_NAME', 'Stock Management');
+
+define('ERROR', 'error');
+
+
+
